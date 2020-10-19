cceee9801d6abcbe89fb591a0671c61ac95ea96f
a-mar-se <a.marquez.seco@hotmail.com>
2020-10-19 22:47:56 +0200

    AddEventListener(KeyPress) & infinite loop on map walls

---
+++ Full-project/src/lib/components/moveSnake.js
@@ -0,0 +1,23 @@
+moveSnake = () => {
+  let snakeHead = this.state.snake[0];
+  const snakeCopy = JSON.parse(JSON.stringify(this.state.snake));
+  switch (this.state.direction) {
+    case 'up':
+      snakeHead = snakeHead - WIDTH;
+      break;
+    case 'down':
+      snakeHead = snakeHead + WIDTH;
+      break;
+    case 'left':
+      snakeHead = snakeHead - 1;
+      break;
+    case 'right':
+      snakeHead = snakeHead + 1;
+      break;
+  }
+  snakeCopy.pop();
+  snakeCopy.unshift(snakeHead);
+  console.log(snakeCopy);
+  return this.setState({ snake: snakeCopy });
+};
+export default moveSnake;
